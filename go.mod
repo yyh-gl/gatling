3a83c0d08e2dfd329bdc1ad782624b38b4818a21
yyh-gl <yhonda.95.gl@gmail.com>
2024-10-06 01:22:58 +0900

    first version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yyh-gl/gatling
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"net/http"
+	"os"
+	"time"
+)
+
+const maxSleepSec int64 = 10
+
+var (
+	isDebugMode    bool
+	peekStartHours = []int{10, 13, 18}
+	peekEndHours   = []int{11, 14, 20}
+)
+
+func main() {
+	isDebugMode = os.Getenv("DEBUG") == "true"
+
+	go gatling(http.MethodGet)
+	go gatling(http.MethodGet)
+	go gatling(http.MethodGet)
+	go gatling(http.MethodGet)
+	go gatling(http.MethodGet)
+
+	go gatling(http.MethodPost)
+	go gatling(http.MethodPost)
+
+	select {}
+}
+
+func gatling(method string) {
+	for i := 0; ; i++ {
+		request(method)
+
+		sleepSec := maxSleepSec
+		nowHour := time.Now().Hour()
+		for j := 0; j < len(peekStartHours); j++ {
+			if peekStartHours[j] <= nowHour && nowHour < peekEndHours[j] {
+				log("in peek time!")
+				sleepSec /= 2
+			}
+		}
+		sleep(sleepSec)
+	}
+}
+
+func request(method string) {
+	targetURL := os.Getenv("TARGET_URL")
+	req, _ := http.NewRequest(method, targetURL, nil)
+	client := http.DefaultClient
+	_, _ = client.Do(req)
+	log(fmt.Sprintf("send %s request", method))
+}
+
+func sleep(maxSleepSec int64) {
+	sec, _ := rand.Int(rand.Reader, big.NewInt(maxSleepSec))
+	log(fmt.Sprintf("sleep %d sec", sec))
+	time.Sleep(time.Duration(sec.Int64()) * time.Second)
+}
+
+func log(message string) {
+	if isDebugMode {
+		fmt.Println(message)
+	}
+}
